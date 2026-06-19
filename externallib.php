ca40c0f9d84e5789619a5ea697a0a0b26c6cb836
Victor Da Silva <victor.da_silva_caseiro@upmc.fr>
2026-06-19 14:09:06 +0200

    fix(externallib): use namespaced core_external classes (no more lib/externallib.php require)

--- externallib.php
+++ externallib.php
@@ -46,7 +46,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->libdir . '/externallib.php');
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_multiple_structure;
+use core_external\external_single_structure;
+use core_external\external_value;
 
 /**
  * External class for the local_su_statboard_api plugin.
